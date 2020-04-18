2e9ce11ce731ee17f898c69c513041e374171a53
Shawn Rivers <kaushou@gmail.com>
2020-04-18 22:40:45 +0900

    Initialize the repository

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import * as React from 'react';
+
+const IndexPage: NextPage = () => {
+  return (
+    <>
+      <h1>Home Page</h1>
+    </>
+  );
+};
+
+export default IndexPage;

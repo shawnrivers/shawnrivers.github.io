915daaafa525e0ab471fa54c6fd4d45d08d8c127
Shawn Rivers <kaushou@gmail.com>
2020-04-19 14:56:49 +0900

    Apply theme background color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,35 @@
+/**@jsx jsx */
+import { jsx, css, Global } from '@emotion/core';
 import { AppProps } from 'next/app';
 import * as React from 'react';
 import { ThemeProvider } from 'emotion-theming';
 import { ThemeKey, themes } from '../theming/themes';
 
 const App = (props: AppProps) => {
-  const [theme, setTheme] = React.useState<ThemeKey>('light');
+  const [themeKey, setThemeKey] = React.useState<ThemeKey>('light');
   const handleSwitchTheme = React.useCallback(() => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
-  }, [theme]);
+    setThemeKey(themeKey === 'light' ? 'dark' : 'light');
+  }, [themeKey]);
+
+  const theme = themes[themeKey];
 
   return (
-    <ThemeProvider theme={themes[theme]}>
-      <button onClick={handleSwitchTheme}>Change Theme</button>
-      <props.Component {...props.pageProps} />
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
+      <div
+        css={css`
+          background-color: ${theme.colors.theme.background.standard};
+        `}
+      >
+        <button onClick={handleSwitchTheme}>Change Theme</button>
+        <props.Component {...props.pageProps} />
+      </div>
     </ThemeProvider>
   );
 };

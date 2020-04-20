e82d654d69a11b8ab37641a095b276b12cb689a1
Shawn Rivers <kaushou@gmail.com>
2020-04-20 22:12:20 +0900

    Add next-images

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+const withImages = require('next-images');
+
 const repository = '/home-page';
 const pathPrefix = process.env.NODE_ENV === 'production' ? repository : '';
 
-module.exports = {
+module.exports = withImages({
   assetPrefix: pathPrefix,
   publicRuntimeConfig: {
     basePath: pathPrefix,
   },
-};
+});

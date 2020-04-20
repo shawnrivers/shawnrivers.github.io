dc4fa8b4f6afafdeb0321af5e7fd1a9784663fcf
Shawn Rivers <kaushou@gmail.com>
2020-04-20 22:28:31 +0900

    Move image files to components directory

--- components/templates/Home.tsx
+++ components/templates/Home.tsx
@@ -5,16 +5,21 @@ import { Typography } from '../../components/atoms/Typography';
 import { useTheme } from '../../theming/themes';
 import { Layout } from '../atoms/Layout';
 import { CircleImage } from '../atoms/CircleImage';
+import profile1x from '../images/profile-1x.jpg';
+import profile2x from '../images/profile-2x.jpg';
+import profile3x from '../images/profile-3x.jpg';
 
 export const Home: React.FC = () => {
   const theme = useTheme();
+
+  // console.log(profile1x, profile2x, profile3x);
 
   return (
     <>
       <Layout element="section" backgroundColor="standard">
         <CircleImage
-          src="/images/profile-1x.jpg"
-          srcSet="/images/profile-1x.jpg 1x, /images/profile-2x.jpg 2x, /images/profile-3x.jpg 3x"
+          src={profile1x}
+          srcSet={`${profile1x} 1x, ${profile2x} 2x, ${profile3x} 3x`}
           alt="Yuxiao He"
         />
         <Typography

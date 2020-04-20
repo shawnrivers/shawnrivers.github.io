93086f9ae39c1b62a38a13906fc52589e127d56b
Shawn Rivers <kaushou@gmail.com>
2020-04-21 01:28:49 +0900

    Add page title and meta data

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
-import NextDocument, { Html, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
+        <Head>
+          <title>Yuxiao He</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />

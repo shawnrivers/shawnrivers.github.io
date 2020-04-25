61cef47bae1df3ec95819cec7502e57c6b3ad782
Shawn Rivers <kaushou@gmail.com>
2020-04-25 22:32:05 +0900

    Fix indent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,56 +31,56 @@ const App = (props: AppProps) => {
       <ThemeProvider theme={theme}>
         <Global
           styles={css`
-          @font-face {
-            font-family: 'IBM Plex Mono';
-            font-weight: normal;
-            font-style: normal;
-            src: url('${basePath}/fonts/IBMPlexMono-Regular.woff2') format('woff2'),
-              url('${basePath}/fonts/IBMPlexMono-Regular.woff') format('woff'),
-              url('${basePath}/fonts/IBMPlexMono-Regular.ttf') format('truetype');
-          }
+            @font-face {
+              font-family: 'IBM Plex Mono';
+              font-weight: normal;
+              font-style: normal;
+              src: url('${basePath}/fonts/IBMPlexMono-Regular.woff2') format('woff2'),
+                url('${basePath}/fonts/IBMPlexMono-Regular.woff') format('woff'),
+                url('${basePath}/fonts/IBMPlexMono-Regular.ttf') format('truetype');
+            }
 
-          @font-face {
-            font-family: 'IBM Plex Mono';
-            font-weight: bold;
-            font-style: normal;
-            src: url('${basePath}/fonts/IBMPlexMono-Bold.woff2') format('woff2'),
-              url('${basePath}/fonts/IBMPlexMono-Bold.woff') format('woff'),
-              url('${basePath}/fonts/IBMPlexMono-Bold.ttf') format('truetype');
-          }
+            @font-face {
+              font-family: 'IBM Plex Mono';
+              font-weight: bold;
+              font-style: normal;
+              src: url('${basePath}/fonts/IBMPlexMono-Bold.woff2') format('woff2'),
+                url('${basePath}/fonts/IBMPlexMono-Bold.woff') format('woff'),
+                url('${basePath}/fonts/IBMPlexMono-Bold.ttf') format('truetype');
+            }
 
-          body {
-            font-family: 'IBM Plex Mono', 'Courier New', Courier, monospace;
-            color: ${theme.colors.theme.primary.standard};
-            background-color: ${theme.colors.theme.background.standard};
-          }
+            body {
+              font-family: 'IBM Plex Mono', 'Courier New', Courier, monospace;
+              color: ${theme.colors.theme.primary.standard};
+              background-color: ${theme.colors.theme.background.standard};
+            }
 
-          body, h1, h2, h3, h4, h5, h6, p, caption, a, div, section, article, main, header, footer, ul, li, ol {
-            font-size: 100%;
-            margin: 0;
-            padding: 0;
-            border: 0;
-          }
+            body, h1, h2, h3, h4, h5, h6, p, caption, a, div, section, article, main, header, footer, ul, li, ol {
+              font-size: 100%;
+              margin: 0;
+              padding: 0;
+              border: 0;
+            }
 
-          ul,
-          ol {
-            list-style: none;
-          }
+            ul,
+            ol {
+              list-style: none;
+            }
 
-          a {
-            color: inherit;
-            text-decoration: none;
-          }
+            a {
+              color: inherit;
+              text-decoration: none;
+            }
 
-          button {
-            appearance: none;
-            background-color: transparent;
-            border: 0;
-            cursor: pointer;
-            font: inherit;
-            outline: initial;
-            padding: 0;
-          }
+            button {
+              appearance: none;
+              background-color: transparent;
+              border: 0;
+              cursor: pointer;
+              font: inherit;
+              outline: initial;
+              padding: 0;
+            }
         `}
         />
         <div

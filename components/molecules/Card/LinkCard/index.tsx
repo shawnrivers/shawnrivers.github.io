1702344c761fcd88446bb647205c8461b46d5fc5
Shawn Rivers <kaushou@gmail.com>
2020-04-26 14:12:53 +0900

    Add LinkCard component

---
+++ components/molecules/Card/LinkCard/index.tsx
@@ -0,0 +1,134 @@
+/**@jsx jsx */
+import { css, jsx, SerializedStyles } from '@emotion/core';
+import * as React from 'react';
+import { CARD_MAX_WIDTH, CARD_MIN_WIDTH } from '../../../../libs/media';
+import { useTheme, ThemeColors } from '../../../../theming/themes';
+import { Typography } from '../../../atoms/Typography';
+import { CircleImage } from '../../../atoms/CircleImage';
+import { ArrowIcon } from '../../../atoms/icons/ArrowIcon';
+import { CircleIcon } from '../../../atoms/CircleIcon';
+
+const iconSize = 128;
+const headingIconYOffset = iconSize / 2;
+
+type LinkCardProps = {
+  to: string;
+  headingIcon: {
+    src: string;
+    srcSet: string;
+    width: number;
+  };
+  badgeIcon?: React.ReactNode;
+  heading: string;
+  body: string;
+  color?: keyof ThemeColors['primary'];
+  backgroundColor?: keyof ThemeColors['background'];
+  margin?: number;
+  customCSS?: SerializedStyles;
+};
+
+export const LinkCard: React.FC<LinkCardProps> = ({
+  to,
+  headingIcon,
+  badgeIcon,
+  heading,
+  body,
+  color = 'standard',
+  backgroundColor = 'standard',
+  margin = 0,
+  customCSS,
+}) => {
+  const theme = useTheme();
+
+  return (
+    <a href={to}>
+      <div
+        css={[
+          css`
+            background-color: ${theme.colors.theme.background[backgroundColor]};
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+            margin: ${margin + headingIconYOffset}px ${margin}px ${margin}px;
+            max-width: ${CARD_MAX_WIDTH}px;
+            min-width: ${CARD_MIN_WIDTH}px;
+            border-radius: 8px;
+            height: auto;
+          `,
+          customCSS,
+        ]}
+      >
+        <div
+          css={css`
+            position: relative;
+          `}
+        >
+          <CircleImage
+            src={headingIcon.src}
+            srcSet={headingIcon.srcSet}
+            alt={heading}
+            width={headingIcon.width}
+            css={css`
+              background-color: ${theme.colors.theme.primary[color]};
+              border: 4px solid ${theme.colors.theme.primary[color]};
+              margin: -${headingIconYOffset}px 0 ${theme.spacing.s}px;
+            `}
+          />
+          {badgeIcon ? (
+            <div
+              css={css`
+                position: absolute;
+                bottom: 18px;
+                right: 0;
+              `}
+            >
+              {badgeIcon}
+            </div>
+          ) : null}
+        </div>
+        <article
+          css={css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0 ${theme.spacing.m}px ${theme.spacing.m}px;
+          `}
+        >
+          <Typography
+            variant="h4"
+            customCSS={css`
+              color: ${theme.colors.theme.primary[color]};
+              margin-bottom: ${theme.spacing.s}px;
+              text-transform: uppercase;
+            `}
+          >
+            {heading}
+          </Typography>
+          <Typography
+            variant="body2"
+            customCSS={css`
+              color: ${theme.colors.theme.primary[color]};
+            `}
+          >
+            {body}
+          </Typography>
+        </article>
+        <div
+          css={css`
+            position: absolute;
+            bottom: -18px;
+            right: -16px;
+          `}
+        >
+          <CircleIcon
+            icon={ArrowIcon}
+            size={40}
+            color="inverse"
+            backgroundColor="inverse"
+          />
+        </div>
+      </div>
+    </a>
+  );
+};

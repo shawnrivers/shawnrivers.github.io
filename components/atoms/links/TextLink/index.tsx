b30e81d571f9021f8037119e169df7e330e896e7
Shawn Rivers <kaushou@gmail.com>
2020-05-24 12:37:22 +0900

    Add TextLink

---
+++ components/atoms/links/TextLink/index.tsx
@@ -0,0 +1,36 @@
+/**@jsx jsx */
+import { jsx, css } from '@emotion/core';
+import * as React from 'react';
+import { useTheme, ThemeColors } from '../../../../theming/themes';
+import { GlobalColor } from '../../../../theming/colors';
+
+export type TextLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  backgroundColor?: keyof ThemeColors['background'] | GlobalColor;
+};
+
+export const TextLink: React.FC<TextLinkProps> = props => {
+  const { backgroundColor = 'variant0', children, ...restProps } = props;
+  const theme = useTheme();
+  const backgroundColors = React.useMemo(
+    () => ({ ...theme.colors.global, ...theme.colors.theme.background }),
+    [theme]
+  );
+
+  return (
+    <a
+      {...restProps}
+      css={css`
+        display: inherit;
+        padding: ${theme.spacing.xs}px ${theme.spacing.s}px;
+        border-radius: 8px;
+        transition: background-color 0.3s ease-out;
+
+        &:hover {
+          background-color: ${backgroundColors[backgroundColor]};
+        }
+      `}
+    >
+      {children}
+    </a>
+  );
+};

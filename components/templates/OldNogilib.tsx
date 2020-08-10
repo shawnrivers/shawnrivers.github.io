75f6dc533425ddfea2a3683ec28679938e830762
Shawn Rivers <kaushou@gmail.com>
2020-08-10 14:38:41 +0900

    Modify redirect page

--- components/templates/OldNogilib.tsx
+++ components/templates/OldNogilib.tsx
@@ -3,40 +3,58 @@ import { jsx, css } from '@emotion/core';
 import * as React from 'react';
 import { Typography } from '../atoms/Typography';
 import { AnchorBase } from '../atoms/buttons/AnchorBase';
+import { Layout } from '../atoms/Layout';
 
 export const OldNogilib: React.FC = () => {
   return (
-    <div
+    <Layout
+      backgroundColor="standard"
       css={css`
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin: auto;
-        max-width: 60rem;
+        height: 100%;
       `}
     >
-      <Typography
-        variant="h1"
+      <div
         css={css`
-          margin-bottom: 1em;
-          text-align: center;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          margin: auto;
+          max-width: 60rem;
         `}
       >
-        NOGILIB
-      </Typography>
-      <Typography
-        variant="h3"
-        element="p"
-        css={css`
-          margin-bottom: 1em;
-          text-align: center;
-        `}
-      >
-        NOGILIB (old name: Nogizaka Lib) has been moved to
-        <AnchorBase href="https://shawnrivers.github.io/nogilib/discography">
-          here
-        </AnchorBase>
-      </Typography>
-    </div>
+        <Typography
+          variant="h1"
+          css={css`
+            margin-top: 1em;
+            text-align: center;
+          `}
+        >
+          NOGILIB
+        </Typography>
+        <Typography
+          variant="body1"
+          color="variant1"
+          css={css`
+            margin-top: 0.5em;
+            text-align: center;
+          `}
+        >
+          (Nogizaka Lib)
+        </Typography>
+        <Typography
+          variant="h3"
+          element="p"
+          css={css`
+            margin-top: 2em;
+            text-align: center;
+          `}
+        >
+          This project has been moved to
+          <AnchorBase href="https://shawnrivers.github.io/nogilib/discography">
+            here
+          </AnchorBase>
+        </Typography>
+      </div>
+    </Layout>
   );
 };
